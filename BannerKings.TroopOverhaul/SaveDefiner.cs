4d7005a5908310d2ba64532008ea80567708e1a6
R. Vaccari <rodrigo.melo@penguinformula.com>
2023-09-24 12:49:31 +0100

    New religions

--- BannerKings.TroopOverhaul/SaveDefiner.cs
+++ BannerKings.TroopOverhaul/SaveDefiner.cs
@@ -1,5 +1,6 @@
 ﻿using TaleWorlds.SaveSystem;
 using BannerKings.CulturesExpanded.Religions;
+using BannerKings.Managers.Institutions.Religions.Faiths.Empire;
 
 namespace BannerKings
 {
@@ -12,6 +13,9 @@ namespace BannerKings
         protected override void DefineClassTypes()
         {
             AddClassDefinition(typeof(Calradism), 1);
+            AddClassDefinition(typeof(LegionariesSarapios), 2);
+            AddClassDefinition(typeof(Junme), 3);
+            AddClassDefinition(typeof(Rodovera), 4);
         }
 
         protected override void DefineContainerDefinitions()
