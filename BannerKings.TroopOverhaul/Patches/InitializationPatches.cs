bdcebd2302693ca909bab4fe08f71fe50c266513
rodrigo <rodrigo.v.melo@hotmail.com>
2023-12-29 13:36:43 +0000

    Init feats

---
+++ BannerKings.TroopOverhaul/Patches/InitializationPatches.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem.CharacterDevelopment;
+using BannerKings.CulturesExpanded.Cultures;
+
+namespace BannerKings.CulturesExpanded.Patches
+{
+    internal class InitializationPatches
+    {
+        [HarmonyPatch(typeof(DefaultCulturalFeats))]
+        public class DefaultCulturalFeatsPatch
+        {
+            [HarmonyPrefix]
+            [HarmonyPatch("InitializeAll")]
+            static void Refresh(DefaultCulturalFeats __instance)
+            {
+                BKCEFeats.Instance.Initialize();
+            }
+        }
+    }
+}
