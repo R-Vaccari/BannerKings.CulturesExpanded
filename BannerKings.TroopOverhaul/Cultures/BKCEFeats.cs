d461ac26a48e41eecadf3c44f0e0f9eac9066a60
rodrigo <rodrigo.v.melo@hotmail.com>
2024-01-01 16:33:02 +0000

    New languages, first culture feats

---
+++ BannerKings.TroopOverhaul/Cultures/BKCEFeats.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using TaleWorlds.CampaignSystem.CharacterDevelopment;
+using TaleWorlds.Core;
+
+namespace BannerKings.CulturesExpanded.Cultures
+{
+    public class BKCEFeats : DefaultTypeInitializer<BKCEFeats, FeatObject>
+    {
+        public FeatObject SailingSpeed { get; set; }
+        public FeatObject SailingBattle { get; set; }
+        public FeatObject Unscholarly { get; set; }
+        public FeatObject VillageMilitia { get; set; }
+        public FeatObject Vakken1 { get; set; }
+        public FeatObject SouthernAgriculture { get; set; }
+        public FeatObject Siri1 { get; set; }
+
+        public override IEnumerable<FeatObject> All => throw new System.NotImplementedException();
+
+        public override void Initialize()
+        {
+            SailingSpeed = Game.Current.ObjectManager.RegisterPresumedObject(new FeatObject("BKCE_sailing_speed"));
+            SailingSpeed.Initialize("BKCE_sailing_speed",
+                "{=!}Naval Tradition: parties are 15% faster while sailing.",
+                0.15f,
+                true,
+            FeatObject.AdditionType.Add);
+
+            SailingBattle = Game.Current.ObjectManager.RegisterPresumedObject(new FeatObject("BKCE_sailing_battle"));
+            SailingBattle.Initialize("BKCE_sailing_battle",
+                "{=!}Naval Tradition: parties are 10% more effective in autoresolve battles while sailing.",
+                0.1f,
+                true,
+                FeatObject.AdditionType.Add);
+
+            Unscholarly = Game.Current.ObjectManager.RegisterPresumedObject(new FeatObject("BKCE_unscholarly"));
+            Unscholarly.Initialize("BKCE_unscholarly",
+                "{=!}Unscholarly Tradition:.",
+                0.2f,
+                true,
+                FeatObject.AdditionType.Add);
+
+            VillageMilitia = Game.Current.ObjectManager.RegisterPresumedObject(new FeatObject("BKCE_village_militia"));
+            VillageMilitia.Initialize("BKCE_village_militia",
+                "{=!}Village Militia Tradition:.",
+                0.2f,
+                true,
+                FeatObject.AdditionType.Add);
+
+            Vakken1 = Game.Current.ObjectManager.RegisterPresumedObject(new FeatObject("BKCE_vakken_production"));
+            Vakken1.Initialize("BKCE_vakken_production",
+                "{=!}Hunter Tradition: forester and trapper villages are 20% more productive.",
+                0.2f,
+                true,
+                FeatObject.AdditionType.Add);
+
+            SouthernAgriculture = Game.Current.ObjectManager.RegisterPresumedObject(new FeatObject("BKCE_south_production"));
+            SouthernAgriculture.Initialize("BKCE_south_production",
+                "{=!}Agriculture Tradition: 25% production bonus to grains, papyrus and dates.",
+                0.2f,
+                true,
+                FeatObject.AdditionType.Add);
+
+            Siri1 = Game.Current.ObjectManager.RegisterPresumedObject(new FeatObject("BKCE_siri_archers"));
+            Siri1.Initialize("BKCE_siri_archers",
+                "{=!}Bow Tradition: ranged units are 40% cheaper to recruit.",
+                -0.4f,
+                true,
+                FeatObject.AdditionType.AddFactor);
+
+            /*
+            DefaultCulturalFeats.BattanianForestSpeedFeat.Initialize("{=!}battanian_forest_speed", 
+                "{=!}Woodland Tradition: 50% less speed penalty and 15% sight range bonus in forests.", 
+                0.5f, 
+                true, 
+                FeatObject.AdditionType.AddFactor);
+
+            DefaultCulturalFeats.BattanianMilitiaFeat.Initialize("{=!}battanian_militia_production", 
+                "{=!}Citizen Militia Tradition: towns have +1 militia production.", 
+                1f, 
+                true, 
+                FeatObject.AdditionType.Add);
+
+            DefaultCulturalFeats.VlandianCastleVillageProductionFeat.Initialize("{=!}vlandian_villages_production_bonus", 
+                "{=!}Buhr Tradition: villages bound to castles (burhs) are 10% more productive.", 
+                0.1f, 
+                true, 
+                FeatObject.AdditionType.AddFactor);
+
+            DefaultCulturalFeats.VlandianRenownMercenaryFeat.Initialize("{=!}vlandian_renown_mercenary_income", 
+                "{=!}Mercenary Tradition: 5% more renown from the battles, 15% more income while serving as a mercenary.", 
+                0.05f, 
+                true, 
+                FeatObject.AdditionType.AddFactor);
+
+            DefaultCulturalFeats.VlandianArmyInfluenceFeat.Initialize("{=!}vlandian_increased_army_influence_cost", 
+                "{=!}Autonomous Lords: recruiting lords to armies costs 20% more influence.", 
+                0.2f, 
+                false, 
+                FeatObject.AdditionType.AddFactor);
+
+            DefaultCulturalFeats.SturgianArmyCohesionFeat.Initialize("{=!}sturgian_decreased_cohesion_rate", 
+                "{=!}Cohesive Leadership: armies lose 20% less daily cohesion.", 
+                -0.2f,
+                true, 
+                FeatObject.AdditionType.AddFactor);*/
+        }
+    }
+}
--- BannerKings.TroopOverhaul/Cultures/BKCELanguages.cs
+++ BannerKings.TroopOverhaul/Cultures/BKCELanguages.cs
@@ -1,4 +1,5 @@
-﻿using BannerKings.Managers.Education.Languages;
+﻿using BannerKings.CulturesExpanded.Religions;
+using BannerKings.Managers.Education.Languages;
 using BannerKings.Managers.Recruits;
 using System;
 using System.Collections.Generic;
@@ -15,6 +16,9 @@ namespace BannerKings.CulturesExpanded.Cultures
         public Language Jumne { get; } = new Language("language_nord");
         public Language Kannic { get; } = new Language("language_kannic");
         public Language Geroia { get; } = new Language("language_geroian");
+        public Language Bragantian { get; } = new Language("language_bragantian");
+        public Language Massa { get; } = new Language("language_massa");
+        public Language Siri { get; } = new Language("language_siri");
 
         public override IEnumerable<Language> All { 
             get 
@@ -23,12 +27,61 @@ namespace BannerKings.CulturesExpanded.Cultures
                 yield return Jumne;
                 yield return Kannic;
                 yield return Geroia;
+                yield return Siri;
+                yield return Massa;
+                yield return Bragantian;
             }
         }
 
         public override void Initialize()
         {
             var cultures = Game.Current.ObjectManager.GetObjectTypeList<CultureObject>();
+
+            DefaultLanguages.Instance.Vlandic.Initialize(new TextObject("{=!}Wilunding"),
+                new TextObject("{=!}The common language spoken in the Wilunding realm, an amalgamation of various dialects. The Wilunding tribes originally hail from Balion, a realm in another continent, where they speak still much similar to their Calradian distant kin."),
+                new List<CultureObject>(1)
+                {
+                    cultures.First(x => x.StringId == "vlandia"),
+                    cultures.First(x => x.StringId == "balion")
+                },
+                new Dictionary<Language, float> 
+                { 
+                    { DefaultLanguages.Instance.Calradian, 0.15f },
+                    { Jumne, 0.4f },
+                    { Massa, 0.7f }
+                });
+
+            Bragantian.Initialize(new TextObject("{=!}Biscanjan"),
+               new TextObject("{=!}Spoken in the Biscan Isles, Biscanjan is the amalgamation of the existing Calradoi dialect, itslef already diverging from the main imperial language, with the Massa settlers of the isles."),
+               new List<CultureObject>(1)
+               {
+                    cultures.First(x => x.StringId == "bragantia")
+               },
+               new Dictionary<Language, float>()
+               {
+                   {  Massa, 0.2f },
+                   {  DefaultLanguages.Instance.Calradian, 0.4f }
+               });
+
+            Massa.Initialize(new TextObject("{=!}Massa"),
+               new TextObject("{=!}Spoken by the Massa tribe, their language is much similar to Vlandic. Most of their vocabulary still revolves around agricultural and warfare themes, lacking in deep philosophical concepts."),
+               new List<CultureObject>(1)
+               {
+                    cultures.First(x => x.StringId == "massa")
+               },
+               new Dictionary<Language, float>()
+               {
+                   {  Jumne, 0.2f },
+                   {  DefaultLanguages.Instance.Vlandic, 0.7f }
+               });
+
+            Siri.Initialize(new TextObject("{=!}Siri"),
+               new TextObject("{=!}The ancient language of the Siri people."),
+               new List<CultureObject>(1)
+               {
+                    cultures.First(x => x.StringId == "siri")
+               },
+               new Dictionary<Language, float>());
 
             Darshi.Initialize(new TextObject("{=IHhWB1aa}Darshi"),
                new TextObject("{=!}A very ancient language, with echoes from a civilized past before the existance of the Calradoi. The Darshi language is natural to the southeast of Calradia, and previously the east, before the Khuzaits conquered their kingdom, mostly composed of cities founded by the Darshi. The language is elaborate, poetic and for the ears of a foreigner, complex. Yet, it has nevertheless gained space in the courts of the Aserai, who have long took aspects of Darshi culture to their own uses."),
@@ -46,7 +99,8 @@ namespace BannerKings.CulturesExpanded.Cultures
                },
                new Dictionary<Language, float>()
                {
-                   { DefaultLanguages.Instance.Vlandic, 0.2f }
+                   { DefaultLanguages.Instance.Vlandic, 0.4f },
+                   { Massa, 0.2f }
                });
 
             Kannic.Initialize(new TextObject("{=!}Kannic"),
