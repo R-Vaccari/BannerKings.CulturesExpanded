e32a12bcd85df9a75aa50aa59eaebf6fcd2d86e9
rodrigo <rodrigo.v.melo@hotmail.com>
2024-01-01 16:42:02 +0000

    Update to bl 1.2

--- BannerKings.TroopOverhaul/Goals/CrownGuardGoal.cs
+++ BannerKings.TroopOverhaul/Goals/CrownGuardGoal.cs
@@ -114,6 +114,7 @@ namespace BannerKings.CulturesExpanded.Goals
                 options,
                 true,
                 1,
+                1,
                 GameTexts.FindText("str_done").ToString(),
                 GameTexts.FindText("str_cancel").ToString(),
                 delegate (List<InquiryElement> selectedOptions)
